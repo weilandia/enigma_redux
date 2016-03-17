066aa40de289bb471a53aea0dfc123a0b6463950
Nick Weiland <nickweiland@gmail.com>
2016-03-17 01:06:35 -0400

    add encryption

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -1,7 +1,42 @@
 class Encryption
+  attr_reader :encryption
+  
   def initialize(message, key, date)
     @offset = Offset.new(key, date).offset
-    require "pry"; binding.pry
     @encryption = encrypt(message)
+  end
+
+  def encrypt(message)
+    chars_translation = translate(message)
+    rotated_chars = add_offset(chars_translation)
+    simple_rotation = simplify_offset(rotated_chars)
+    encrypted(simple_rotation)
+  end
+
+  def translate(message)
+    characters = map_characters
+    message.chars.map do |letter|
+      characters.index(letter)
+    end
+  end
+
+  def add_offset(translation)
+    translation.zip(@offset.cycle).map do |rotation|
+      rotation.inject(:+)
+    end
+  end
+
+  def simplify_offset(rotated_chars)
+    rotated_chars.map do |char|
+      char % map_characters.length
+    end
+  end
+
+  def encrypted(simple_rotation)
+    simple_rotation.map { |char_index| map_characters[char_index] }.join
+  end
+
+  def map_characters
+    [*"A".."z", *"0".."9", ",", ".", "!", "?", "\"", "\'", " "]
   end
 end
--- spec/encryption_spec.rb
+++ spec/encryption_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 class EncryptionTest < Minitest::Test
   def test_encryption_encrypts_four_letters_correctly
-    assert_equal "hey", Encryption.new("abcd", "12345", 111111).encryption
+    assert_equal "X4EO4Z0R7.?j", Encryption.new("Hello World!", "12345", 111111).encryption
   end
 end
